c155171576a692be0f6236e9ed031e37c9957079
Sami Koskivaara <fisamkosk3@fihava.local>
2020-01-15 14:05:30 +0200

    Add GitHub actions

--- publish.ps1
+++ publish.ps1
@@ -1,14 +1,3 @@
-# For local Windows workstation usage only
-# TODO: Create generic publisher for GitHub actions CI/CD
-
 $moduleName = 'ServiceNow-MIDServer'
-$moduleRoot = "$($env:SystemDrive)\Users\$($env:USERNAME)\Documents\PowerShell\Modules"
-
 $moduleSrcPath = Join-Path -Path $PSScriptRoot -ChildPath $moduleName
-$configFilePath = Join-Path -Path $moduleSrcPath -ChildPath "$($moduleName).psd1"
-
-$config = Import-PowerShellDataFile $configFilePath
-
-$installPath = Join-Path -Path (Join-Path $moduleRoot $moduleName) -ChildPath $config.ModuleVersion
-Copy-Item -Path $moduleSrcPath -Destination $installPath -Recurse -Force
-Publish-Module -Name $moduleName -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY
+Publish-Module -Path $moduleSrcPath -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY -WhatIf
