87f79ed822fd8f82f520a1d30320ff979229d5b4
Sami Koskivaara <fisamkosk3@fihava.local>
2020-01-15 14:49:51 +0200

    ff

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
 $moduleName = 'ServiceNow-MIDServer'
 $moduleSrcPath = Join-Path -Path $PSScriptRoot -ChildPath $moduleName
-Publish-Module -Path $moduleSrcPath -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY
+Publish-Module -Path $moduleSrcPath -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY 
