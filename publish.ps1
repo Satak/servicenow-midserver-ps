60fa82ba463f9a7feec69d3d76aea181e120d2d7
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-01-15 14:50:32 +0200

    remove what if

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
 $moduleName = 'ServiceNow-MIDServer'
 $moduleSrcPath = Join-Path -Path $PSScriptRoot -ChildPath $moduleName
-Publish-Module -Path $moduleSrcPath -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY -WhatIf
+Publish-Module -Path $moduleSrcPath -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY
